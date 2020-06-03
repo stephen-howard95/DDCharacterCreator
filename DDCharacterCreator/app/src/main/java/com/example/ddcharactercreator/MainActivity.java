484dc8aea82b8c080b7c1028bf37bb5b8e9b3695
stephen-howard95 <55605083+stephen-howard95@users.noreply.github.com>
2020-06-02 21:44:22 -0500

    Update MainActivity.java

--- DDCharacterCreator/app/src/main/java/com/example/ddcharactercreator/MainActivity.java
+++ DDCharacterCreator/app/src/main/java/com/example/ddcharactercreator/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.ddcharactercreator;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
-
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -10,7 +14,6 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
@@ -22,12 +25,17 @@ import java.util.List;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements android.app.LoaderManager.LoaderCallbacks<List<Spell>> {
 
-    private CharacterAdapter mAdapter;
+    private CharacterAdapter mCharacterAdapter;
     private MutableLiveData<List<Character>> listOfCharacters = new MutableLiveData<List<Character>>();
 
     private static final String TAG = MainActivity.class.getSimpleName();
+
+    private static final String SPELL_API_BASE_URL = "https://api.open5e.com/spells/";
+    private static final int SPELL_LOADER_ID = 1;
+    private SpellAdapter mSpellAdapter;
+    private SpellDatabase mSpellDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,9 +51,30 @@ public class MainActivity extends AppCompatActivity {
                 .build();
         mAdView.loadAd(adRequest);
 
-        mAdapter = new CharacterAdapter(this, new ArrayList<Character>());
+        mSpellDb = SpellDatabase.getInstance(getApplicationContext());
+        mSpellAdapter = new SpellAdapter(this, new ArrayList<Spell>());
+        if(mSpellDb.spellDao().loadAllSpells().getValue() == null){
+            ConnectivityManager connMgr = (ConnectivityManager)
+                    getSystemService(Context.CONNECTIVITY_SERVICE);
+
+            // Get details on the currently active default data network
+            NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+            // If there is a network connection, fetch data
+            if (networkInfo != null && networkInfo.isConnected()) {
+                // Get a reference to the LoaderManager, in order to interact with loaders.
+                android.app.LoaderManager loaderManager = getLoaderManager();
+
+                // Initialize the loader. Pass in the int ID constant defined above and pass in null for
+                // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
+                // because this activity implements the LoaderCallbacks interface).
+                loaderManager.initLoader(SPELL_LOADER_ID, null, this);
+            }
+        }
+
+        mCharacterAdapter = new CharacterAdapter(this, new ArrayList<Character>());
         ListView characterListView = findViewById(R.id.character_list);
-        characterListView.setAdapter(mAdapter);
+        characterListView.setAdapter(mCharacterAdapter);
         characterListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -53,8 +82,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        CharacterDatabase mDb = CharacterDatabase.getInstance(getApplicationContext());
-        LiveData<List<Character>> savedCharacterList = mDb.characterDao().loadAllCharacters();
+        CharacterDatabase mCharacterDb = CharacterDatabase.getInstance(getApplicationContext());
+        LiveData<List<Character>> savedCharacterList = mCharacterDb.characterDao().loadAllCharacters();
         savedCharacterList.observe(this, new Observer<List<Character>>() {
             @Override
             public void onChanged(List<Character> characters) {
@@ -65,8 +94,8 @@ public class MainActivity extends AppCompatActivity {
         listOfCharacters.observe(this, new Observer<List<Character>>() {
             @Override
             public void onChanged(List<Character> characters) {
-                mAdapter.clear();
-                mAdapter.addAll(characters);
+                mCharacterAdapter.clear();
+                mCharacterAdapter.addAll(characters);
             }
         });
 
@@ -99,7 +128,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onChanged(List<Character> characters) {
                 Log.d(TAG, "Receiving database update from LiveData");
-                mAdapter.setCharacters(characters);
+                mCharacterAdapter.setCharacters(characters);
             }
         });
     }
@@ -124,5 +153,27 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public Loader<List<Spell>> onCreateLoader(int id, Bundle args) {
+        Uri baseUri = Uri.parse(SPELL_API_BASE_URL);
+        return new SpellLoader(this, baseUri.toString());
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<Spell>> loader, List<Spell> spells) {
+        mSpellAdapter.clear();
+        if(spells != null && !spells.isEmpty()){
+            for(int i=0; i<spells.size(); i++){
+                mSpellDb.spellDao().insertSpell(spells.get(i));
+            }
+        }
+        getLoaderManager().destroyLoader(loader.getId());
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<Spell>> loader) {
+        mSpellAdapter.clear();
     }
 }
