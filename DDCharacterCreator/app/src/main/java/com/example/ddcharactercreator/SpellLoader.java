a7970d28ffc89d9088c9952db85bef84f1dcf21f
stephen-howard95 <55605083+stephen-howard95@users.noreply.github.com>
2020-06-03 16:29:31 -0500

    Update SpellLoader.java

--- DDCharacterCreator/app/src/main/java/com/example/ddcharactercreator/SpellLoader.java
+++ DDCharacterCreator/app/src/main/java/com/example/ddcharactercreator/SpellLoader.java
@@ -2,7 +2,6 @@ package com.example.ddcharactercreator;
 
 import android.content.AsyncTaskLoader;
 import android.content.Context;
-
 import java.util.List;
 
 public class SpellLoader extends AsyncTaskLoader {
