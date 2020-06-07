3110ad4d584a991a7bde822c9fb0f6c47de92253
stephen-howard95 <55605083+stephen-howard95@users.noreply.github.com>
2020-06-07 15:48:25 -0500

    Update SpellChooserActivity.java

--- DDCharacterCreator/app/src/main/java/com/example/ddcharactercreator/SpellChooserActivity.java
+++ DDCharacterCreator/app/src/main/java/com/example/ddcharactercreator/SpellChooserActivity.java
@@ -10,33 +10,115 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.example.ddcharactercreator.DetailActivity.calculateModifier;
+
 public class SpellChooserActivity extends AppCompatActivity{
 
     private static Spell spell;
     private Character character = DetailActivity.character;
+
+    private ArrayList<Spell> spellsList = new ArrayList<Spell>();
+    private ArrayList<Spell> cantripsList = new ArrayList<Spell>();
+    private int spellMax;
+    private int cantripMax;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spell);
 
+        switch(character.getCharacterClass()){
+            case "Bard":
+                spellMax = 4;
+                cantripMax = 2;
+                break;
+            case "Cleric":
+                spellMax = character.getLevel() + calculateModifier(character.getStatValues().get(4));
+                cantripMax = 3;
+                break;
+            case "Druid":
+                spellMax = character.getLevel() + calculateModifier(character.getStatValues().get(4));
+                cantripMax = 2;
+                break;
+            case "Paladin":
+                spellMax = character.getLevel()/2 + calculateModifier(character.getStatValues().get(5));
+                cantripMax = -2;
+                break;
+            case "Ranger":
+                spellMax = 2;
+                cantripMax = -2;
+                break;
+            case "Sorcerer":
+                spellMax = 2;
+                cantripMax = 4;
+                break;
+            case "Warlock":
+                spellMax = 2;
+                cantripMax = 2;
+                break;
+            case "Wizard":
+                spellMax = character.getLevel() + calculateModifier(character.getStatValues().get(3));
+                cantripMax = 3;
+        }
+        if(spellMax < 1){
+            spellMax = 1;
+        }
+
+        ArrayList<Spell> characterSpellsList = character.getSpellsKnown();
+
+        // Separates Cantrips and Spells
+        for(int i = 0; i< characterSpellsList.size(); i++){
+            Spell spell = characterSpellsList.get(i);
+            if(spell.getLevel() == 0 && !cantripsList.contains(spell)){
+                cantripsList.add(spell);
+            } else if(!cantripsList.contains(spell) && !spellsList.contains(spell)){
+                spellsList.add(spell);
+            }
+        }
+
         SpellDatabase mDb = SpellDatabase.getInstance(getApplicationContext());
-        List<Spell> spellsList = mDb.spellDao().loadAllSpells();
+        List<Spell> completeSpellsList = mDb.spellDao().loadAllSpells();
 
         SpellAdapter mAdapter = new SpellAdapter(this, new ArrayList<Spell>());
 
         ListView spellListView = findViewById(R.id.list);
 
         spellListView.setAdapter(mAdapter);
-        //Lmits spells available based on character level and character class
-        if(spellsList != null && !spellsList.isEmpty() || !spellsList.get(i).getClassList().contains(character.getCharacterClass())){
-            for(int i = 0; i< spellsList.size(); i++){
-                if(2* spellsList.get(i).getLevel()-1 > character.getLevel()){
-                    spellsList.remove(i);
-                    i--;
+
+        //Limits the available spells for the character based on their level and character class
+        if(completeSpellsList != null && !completeSpellsList.isEmpty()){
+            if(character.getCharacterClass().equals("Ranger") || character.getCharacterClass().equals("Paladin")){
+                for(int i = 0; i < completeSpellsList.size(); i++){
+                    if(4*completeSpellsList.get(i).getLevel()-3 > character.getLevel() || !completeSpellsList.get(i).getClassList().contains(character.getCharacterClass())){
+                        completeSpellsList.remove(i);
+                        i--;
+                    }
+                }
+            }else{
+                for(int i = 0; i< completeSpellsList.size(); i++){
+                    if(2*completeSpellsList.get(i).getLevel()-1 > character.getLevel() || !completeSpellsList.get(i).getClassList().contains(character.getCharacterClass())){
+                        completeSpellsList.remove(i);
+                        i--;
+                    }
                 }
             }
-            mAdapter.addAll(spellsList);
+            if(cantripsList.size() >= cantripMax){
+                for(int i = 0; i< completeSpellsList.size(); i++){
+                    if(completeSpellsList.get(i).getLevel() == 0){
+                        completeSpellsList.remove(i);
+                        i--;
+                    }
+                }
+            }
+            if(spellsList.size() >= spellMax){
+                for(int i = 0; i< completeSpellsList.size(); i++){
+                    if(completeSpellsList.get(i).getLevel() >= 1){
+                        completeSpellsList.remove(i);
+                        i--;
+                    }
+                }
+            }
+            mAdapter.addAll(completeSpellsList);
         } else{
             Toast.makeText(getApplicationContext(), "Spell Database is empty. Try again later", Toast.LENGTH_LONG).show();
         }
