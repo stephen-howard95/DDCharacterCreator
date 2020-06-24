7b5f117a9d512e2b22403d45f9545601cdf96af9
stephen-howard95 <55605083+stephen-howard95@users.noreply.github.com>
2020-06-24 15:04:05 -0500

    Update SpellDetailActivity.java

--- DDCharacterCreator/app/src/main/java/com/example/ddcharactercreator/SpellDetailActivity.java
+++ DDCharacterCreator/app/src/main/java/com/example/ddcharactercreator/SpellDetailActivity.java
@@ -22,19 +22,19 @@ public class SpellDetailActivity extends AppCompatActivity{
         TextView nameTextView = findViewById(R.id.spell_name);
         TextView levelTextView = findViewById(R.id.spell_level);
         TextView durationTextView = findViewById(R.id.spell_duration);
-        TextView concentrationTextView = findViewById(R.id.spell_concentration);
         TextView castingTimeTextView = findViewById(R.id.spell_casting_time);
         TextView rangeTextView = findViewById(R.id.spell_range);
         TextView descriptionTextView = findViewById(R.id.spell_description);
 
         nameTextView.setText(spell.getSpellName());
-        levelTextView.setText(String.valueOf(spell.getLevel()));
-        durationTextView.setText(spell.getDuration());
-        castingTimeTextView.setText(spell.getCastingTime());
-        rangeTextView.setText(spell.getRange());
-        descriptionTextView.setText(spell.getDescription() + "\n" + spell.getHigherLevel());
+        levelTextView.setText("Level " + String.valueOf(spell.getLevel()));
         if(spell.getConcentration().equals("yes")){
-            concentrationTextView.setText(R.string.concentration);
+            durationTextView.setText("Duration: " + getString(R.string.concentration) + spell.getDuration());
+        } else{
+            durationTextView.setText("Duration: " + spell.getDuration());
         }
+        castingTimeTextView.setText("Casting Time: " + spell.getCastingTime());
+        rangeTextView.setText("Range: " + spell.getRange());
+        descriptionTextView.setText(spell.getDescription() + "\n" + spell.getHigherLevel());
     }
 }
