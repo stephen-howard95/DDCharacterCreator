5a186ece4a01d8086dfafe370f578e53f9e93393
stephen-howard95 <55605083+stephen-howard95@users.noreply.github.com>
2020-07-19 11:48:34 -0500

    Update SpellDetailActivity.java

--- DDCharacterCreator/app/src/main/java/com/example/ddcharactercreator/SpellDetailActivity.java
+++ DDCharacterCreator/app/src/main/java/com/example/ddcharactercreator/SpellDetailActivity.java
@@ -25,16 +25,44 @@ public class SpellDetailActivity extends AppCompatActivity{
         TextView castingTimeTextView = findViewById(R.id.spell_casting_time);
         TextView rangeTextView = findViewById(R.id.spell_range);
         TextView descriptionTextView = findViewById(R.id.spell_description);
+        TextView schoolTextView = findViewById(R.id.spell_school);
 
         nameTextView.setText(spell.getSpellName());
-        levelTextView.setText("Level " + String.valueOf(spell.getLevel()));
+        levelTextView.setText(String.format("Level: %s", spell.getLevel()));
         if(spell.getConcentration().equals("yes")){
-            durationTextView.setText("Duration: " + getString(R.string.concentration) + spell.getDuration());
+            durationTextView.setText(String.format("Duration: Concentration, %s", spell.getDuration()));
         } else{
-            durationTextView.setText("Duration: " + spell.getDuration());
+            durationTextView.setText(String.format("Duration: %s", spell.getDuration()));
         }
-        castingTimeTextView.setText("Casting Time: " + spell.getCastingTime());
-        rangeTextView.setText("Range: " + spell.getRange());
+        castingTimeTextView.setText(String.format("Casting Time: %s", spell.getCastingTime()));
+        rangeTextView.setText(String.format("Range: %s", spell.getRange()));
+        schoolTextView.setText(String.format("School: %s", spell.getSchool()));
+        switch(spell.getSchool()){
+            case "Abjuration":
+                schoolTextView.setTextColor(getResources().getColor(R.color.abjuration));
+                break;
+            case "Conjuration":
+                schoolTextView.setTextColor(getResources().getColor(R.color.conjuration));
+                break;
+            case "Divination":
+                schoolTextView.setTextColor(getResources().getColor(R.color.divination));
+                break;
+            case "Enchantment":
+                schoolTextView.setTextColor(getResources().getColor(R.color.enchantment));
+                break;
+            case "Evocation":
+                schoolTextView.setTextColor(getResources().getColor(R.color.evocation));
+                break;
+            case "Illusion":
+                schoolTextView.setTextColor(getResources().getColor(R.color.illusion));
+                break;
+            case "Necromancy":
+                schoolTextView.setTextColor(getResources().getColor(R.color.necromancy));
+                break;
+            case "Transmutation":
+                schoolTextView.setTextColor(getResources().getColor(R.color.transmutation));
+                break;
+        }
         descriptionTextView.setText(spell.getDescription() + "\n" + spell.getHigherLevel());
     }
 }
