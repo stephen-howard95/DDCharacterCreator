192124e648c1cbd3db690858d979a6875729bd07
stephen-howard95 <55605083+stephen-howard95@users.noreply.github.com>
2020-07-30 16:24:38 -0500

    Update ListAdapter.java

--- DDCharacterCreator/app/src/main/java/com/example/ddcharactercreator/ListAdapter.java
+++ DDCharacterCreator/app/src/main/java/com/example/ddcharactercreator/ListAdapter.java
@@ -8,21 +8,45 @@ import android.widget.ArrayAdapter;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
 import java.util.ArrayList;
 
-public class ListAdapter extends ArrayAdapter<String> {
+public class ListAdapter extends RecyclerView.Adapter<ListAdapter.StringViewHolder> {
+
+    private Context context;
+    private ArrayList<String> strings;
+
     public ListAdapter(@NonNull Context context, ArrayList<String> strings) {
-        super(context, 0, strings);
+        this.context = context;
+        this.strings = strings;
     }
 
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent){
-        View view = convertView;
-        if(view == null){
-            view = LayoutInflater.from(getContext()).inflate(R.layout.string_list_item_layout, parent, false);
+    @NonNull
+    @Override
+    public StringViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new StringViewHolder(LayoutInflater.from(context).inflate(R.layout.string_list_item_layout, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull StringViewHolder holder, int position) {
+        String item = strings.get(position);
+        holder.listItemTextView.setText(item);
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class StringViewHolder extends RecyclerView.ViewHolder{
+        private TextView listItemTextView;
+        private View parentView;
+
+        public StringViewHolder(@NonNull View view){
+            super(view);
+            this.parentView = view;
+            this.listItemTextView = view.findViewById(R.id.string_list_item);
         }
-        String item = getItem(position);
-        TextView listItemTextView = view.findViewById(R.id.string_list_item);
-        listItemTextView.setText(item);
-        return view;
     }
 }
