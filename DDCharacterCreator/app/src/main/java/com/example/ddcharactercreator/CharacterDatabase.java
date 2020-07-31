c45fddb5f11ba6c529116a125c84457bb9d82c85
stephen-howard95 <55605083+stephen-howard95@users.noreply.github.com>
2020-07-31 14:04:19 -0500

    Update CharacterDatabase.java

--- DDCharacterCreator/app/src/main/java/com/example/ddcharactercreator/CharacterDatabase.java
+++ DDCharacterCreator/app/src/main/java/com/example/ddcharactercreator/CharacterDatabase.java
@@ -1,11 +1,14 @@
 package com.example.ddcharactercreator;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.util.Log;
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.room.TypeConverters;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 @Database(entities = {Character.class}, version = 1, exportSchema = false)
 @TypeConverters({ArrayListIntegerConverter.class, ArrayListStringConverter.class, ArrayListSpellConverter.class})
@@ -15,6 +18,7 @@ public abstract class CharacterDatabase extends RoomDatabase {
     private static final Object LOCK = new Object();
     private static final String DATABASE_NAME = "Character List";
     private static CharacterDatabase sInstance;
+    public abstract CharacterDao characterDao();
 
     public static CharacterDatabase getInstance(Context context){
         if(sInstance == null){
@@ -23,6 +27,7 @@ public abstract class CharacterDatabase extends RoomDatabase {
                 sInstance = Room.databaseBuilder(context.getApplicationContext(),
                         CharacterDatabase.class, CharacterDatabase.DATABASE_NAME)
                         .allowMainThreadQueries()
+                        .addCallback(roomCallback)
                         .build();
             }
         }
@@ -30,5 +35,23 @@ public abstract class CharacterDatabase extends RoomDatabase {
         return sInstance;
     }
 
-    public abstract CharacterDao characterDao();
+    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback(){
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new PopulateDbAsyncTask(sInstance).execute();
+        }
+    };
+
+    private static class PopulateDbAsyncTask extends AsyncTask<Void, Void, Void>{
+        private CharacterDao characterDao;
+
+        private PopulateDbAsyncTask(CharacterDatabase db){
+            characterDao = db.characterDao();
+        }
+        @Override
+        protected Void doInBackground(Void... voids) {
+            return null;
+        }
+    }
 }
