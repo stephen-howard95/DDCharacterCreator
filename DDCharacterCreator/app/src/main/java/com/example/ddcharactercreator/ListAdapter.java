3f858fb897676ac348bfbb1e4e795b92f6490315
stephen-howard95 <55605083+stephen-howard95@users.noreply.github.com>
2020-07-31 14:11:20 -0500

    Update ListAdapter.java

--- DDCharacterCreator/app/src/main/java/com/example/ddcharactercreator/ListAdapter.java
+++ DDCharacterCreator/app/src/main/java/com/example/ddcharactercreator/ListAdapter.java
@@ -1,25 +1,26 @@
 package com.example.ddcharactercreator;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
-
+import android.app.AlertDialog;
 import java.util.ArrayList;
 
 public class ListAdapter extends RecyclerView.Adapter<ListAdapter.StringViewHolder> {
 
     private Context context;
     private ArrayList<String> strings;
+    private Boolean canDelete;
 
-    public ListAdapter(@NonNull Context context, ArrayList<String> strings) {
+    public ListAdapter(@NonNull Context context, ArrayList<String> strings, Boolean canDelete) {
         this.context = context;
         this.strings = strings;
+        this.canDelete = canDelete;
     }
 
     @NonNull
@@ -32,11 +33,31 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.StringViewHold
     public void onBindViewHolder(@NonNull StringViewHolder holder, int position) {
         String item = strings.get(position);
         holder.listItemTextView.setText(item);
+        if(canDelete){
+            holder.parentView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    AlertDialog.Builder adb = new AlertDialog.Builder(context);
+                    adb.setTitle("Delete?");
+                    adb.setMessage("Are you sure you want to delete " + item);
+                    final int positionToRemove = position;
+                    adb.setNegativeButton("Cancel", null);
+                    adb.setPositiveButton("Ok", new AlertDialog.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            strings.remove(positionToRemove);
+                            notifyDataSetChanged();
+                        }
+                    });
+                    adb.show();
+                    return true;
+                }
+            });
+        }
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return this.strings.size();
     }
 
     public class StringViewHolder extends RecyclerView.ViewHolder{
