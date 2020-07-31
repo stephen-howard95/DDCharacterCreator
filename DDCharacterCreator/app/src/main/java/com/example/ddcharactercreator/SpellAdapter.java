a401dc016d245c17d68a9a9f5a60570a7c22efb3
stephen-howard95 <55605083+stephen-howard95@users.noreply.github.com>
2020-07-31 14:25:31 -0500

    Update SpellAdapter.java

--- DDCharacterCreator/app/src/main/java/com/example/ddcharactercreator/SpellAdapter.java
+++ DDCharacterCreator/app/src/main/java/com/example/ddcharactercreator/SpellAdapter.java
@@ -1,34 +1,118 @@
 package com.example.ddcharactercreator;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
+import android.app.AlertDialog;
+import android.widget.Toast;
 
-public class SpellAdapter extends ArrayAdapter<Spell> {
-    public SpellAdapter(@NonNull Context context, ArrayList<Spell> spells) {
-        super(context, 0, spells);
+public class SpellAdapter extends RecyclerView.Adapter<SpellAdapter.SpellViewHolder> {
+
+    private Context context;
+    private ArrayList<Spell> spells;
+    private Boolean isDatabaseSpell;
+
+    public SpellAdapter(@NonNull Context context, ArrayList<Spell> spells, Boolean isDatabaseSpell) {
+        this.context = context;
+        this.spells = spells;
+        this.isDatabaseSpell = isDatabaseSpell;
     }
 
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent){
-        View spellView = convertView;
-        if (spellView == null){
-            spellView = LayoutInflater.from(getContext()).inflate(R.layout.spell_list_item, parent, false);
-        }
-        Spell spell = getItem(position);
-        TextView spellNameTextView = spellView.findViewById(R.id.spell_name);
-        spellNameTextView.setText(spell.getSpellName());
+    @NonNull
+    @Override
+    public SpellAdapter.SpellViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new SpellViewHolder(LayoutInflater.from(context).inflate(R.layout.spell_list_item, parent, false));
+    }
 
-       if(spell.getLevel() != 0){
-            TextView spellLevelTextView = spellView.findViewById(R.id.spell_level);
-            spellLevelTextView.setText(String.valueOf(spell.getLevel()));
+    @Override
+    public void onBindViewHolder(@NonNull SpellViewHolder holder, int position) {
+        Spell spell = spells.get(position);
+        holder.spellNameTextView.setText(spell.getSpellName());
+        if(spell.getLevel() > 0){
+            holder.spellLevelTextView.setText(String.valueOf(spell.getLevel()));
+        } else{
+            holder.spellLevelTextView.setVisibility(View.GONE);
         }
+        holder.parentView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, SpellDetailActivity.class);
+                intent.putExtra(SpellDetailActivity.SPELL, spell);
+                context.startActivity(intent);
+            }
+        });
+        if(isDatabaseSpell){
+            holder.parentView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    Character character = DetailActivity.character;
+                    //prevents duplicate spells from being added to the list and warns the user
+                    int i=0;
+                    boolean spellIsUnique = true;
+                    do{
+                        if(character.getSpellsKnown().size() == 0){
+                            break;
+                        }
+                        Spell knownSpell = character.getSpellsKnown().get(i);
+                        if(spell.getSpellName().equals(knownSpell.getSpellName())){
+                            Toast.makeText(context, "You already know this spell", Toast.LENGTH_SHORT).show();
+                            spellIsUnique = false;
+                            break;
+                        }
+                        i++;
+                    } while(i<character.getSpellsKnown().size());
+                    if(spellIsUnique){
+                        character.getSpellsKnown().add(spell);
+                        ((Activity) context).finish();
+                        return true;
+                    }
+                    return false;
+                }
+            });
+        }else{
+            holder.parentView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    AlertDialog.Builder adb=new AlertDialog.Builder(context);
+                    adb.setTitle("Delete?");
+                    adb.setMessage("Are you sure you want to delete " + spell.getSpellName());
+                    final int positionToRemove = position;
+                    adb.setNegativeButton("Cancel", null);
+                    adb.setPositiveButton("Ok", new AlertDialog.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            spells.remove(positionToRemove);
+                            notifyDataSetChanged();
+                        }});
+                    adb.show();
+                    return true;
+                }
+            });
+        }
+    }
 
-        return spellView;
+    @Override
+    public int getItemCount() {
+        return spells.size();
+    }
+
+    public class SpellViewHolder extends RecyclerView.ViewHolder{
+        private TextView spellNameTextView;
+        private TextView spellLevelTextView;
+        private View parentView;
+
+        public SpellViewHolder(@NonNull View view){
+            super(view);
+            this.parentView = view;
+            this.spellNameTextView = view.findViewById(R.id.spell_name);
+            this.spellLevelTextView = view.findViewById(R.id.spell_level);
+        }
     }
 }
