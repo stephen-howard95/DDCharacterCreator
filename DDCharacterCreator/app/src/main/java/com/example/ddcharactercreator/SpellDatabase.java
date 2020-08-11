9a02564308f0042c99e9fda8f148354b29ce6ba9
stephen-howard95 <55605083+stephen-howard95@users.noreply.github.com>
2020-08-11 16:12:47 -0500

    Update SpellDatabase.java

--- DDCharacterCreator/app/src/main/java/com/example/ddcharactercreator/SpellDatabase.java
+++ DDCharacterCreator/app/src/main/java/com/example/ddcharactercreator/SpellDatabase.java
@@ -1,12 +1,13 @@
 package com.example.ddcharactercreator;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.util.Log;
-
-import androidx.room.Dao;
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 @Database(entities = {Spell.class}, version = 1, exportSchema = false)
 public abstract class SpellDatabase extends RoomDatabase {
@@ -15,6 +16,7 @@ public abstract class SpellDatabase extends RoomDatabase {
     private static final Object LOCK = new Object();
     private static final String DATABASE_NAME = "Spell List";
     private static SpellDatabase sInstance;
+    public abstract SpellDao spellDao();
 
     public static SpellDatabase getInstance(Context context){
         if(sInstance == null){
@@ -22,7 +24,7 @@ public abstract class SpellDatabase extends RoomDatabase {
                 Log.d(LOG_TAG, "Creating new database instance");
                 sInstance = Room.databaseBuilder(context.getApplicationContext(),
                         SpellDatabase.class, SpellDatabase.DATABASE_NAME)
-                        .allowMainThreadQueries()
+                        .addCallback(roomCallback)
                         .build();
             }
         }
@@ -30,5 +32,23 @@ public abstract class SpellDatabase extends RoomDatabase {
         return sInstance;
     }
 
-    public abstract SpellDao spellDao();
+    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback(){
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new PopulateDbAsyncTask(sInstance).execute();
+        }
+    };
+
+    private static class PopulateDbAsyncTask extends AsyncTask<Void, Void, Void> {
+        private SpellDao spellDao;
+
+        private PopulateDbAsyncTask(SpellDatabase db){
+            spellDao = db.spellDao();
+        }
+        @Override
+        protected Void doInBackground(Void... voids) {
+            return null;
+        }
+    }
 }
